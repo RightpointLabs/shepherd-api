7247bc5dd7cfcb458b73cd201c3b4744d24345df
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-11-03 19:54:19 -0500

    Updated response

--- functions/GetTopics.cs
+++ functions/GetTopics.cs
@@ -19,15 +19,7 @@ namespace functions
         {
             log.LogInformation("Getting all Topics.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return (ActionResult)new OkObjectResult(Data.Topics);
         }
     }
 }
