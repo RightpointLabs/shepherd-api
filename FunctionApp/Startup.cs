bcba016754b49b52759b990df4759aa40cef379c
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-11-04 18:26:48 -0600

    Beginning to remove middle domain layer

--- FunctionApp/Startup.cs
+++ FunctionApp/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
+using Gremlin.Net.CosmosDb;
 
 [assembly: WebJobsStartup(typeof(Startup))]
 namespace FunctionApp
@@ -15,7 +16,11 @@ namespace FunctionApp
 
         private void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IRepository, MockRepository>();
+            IGraphClient graphClient = new GraphClient("shepherd-local.gremlin.cosmosdb.azure.com", "shepherd", "shepherd", "rjfqIQKhqBcpMarT0alwQJdpgVQopBDzZi1IZpso2v1z0A3fTEKyUOIZPdaQ10XnoKNFpLgrpmw4wlHsdFb76A==");
+            services.AddSingleton(graphClient);
+
+            // IRepository repository = new AzureCosmosGraphRepository("shepherd-local.gremlin.cosmosdb.azure.com", "shepherd", "shepherd", "rjfqIQKhqBcpMarT0alwQJdpgVQopBDzZi1IZpso2v1z0A3fTEKyUOIZPdaQ10XnoKNFpLgrpmw4wlHsdFb76A==");
+            // services.AddSingleton(repository);
         }
     }
 }
