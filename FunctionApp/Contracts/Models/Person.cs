636be6e74741719c0d571c57610678c30b2d34ba
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2019-01-26 15:41:54 -0600

    Contract objects

---
+++ FunctionApp/Contracts/Models/Person.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace FunctionApp.Contracts.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
