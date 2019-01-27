daf208dbf18c342d745286a11973a81aa3e2d62b
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2019-01-27 09:23:26 -0600

    Connection string ref

--- FunctionApp/Startup.cs
+++ FunctionApp/Startup.cs
@@ -1,20 +1,22 @@
+using System;
 using FunctionApp;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
 
 [assembly: WebJobsStartup(typeof(Startup))]
 namespace FunctionApp
 {
     internal class Startup : IWebJobsStartup
     {
-        public void Configure(IWebJobsBuilder builder) =>
-            builder.AddDependencyInjection(ConfigureServices);
+        public void Configure(IWebJobsBuilder builder) { }
 
         private void ConfigureServices(IServiceCollection services)
         {
             // register EF
+            var connection = Environment.GetEnvironmentVariable("ConnectionString", EnvironmentVariableTarget.Process); ;
+            services.AddDbContext<Shared.Persistence.ShepherdContext>(options => options.UseSqlServer(connection));
         }
     }
 }
